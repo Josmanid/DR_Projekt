665b7406c02dc3dfbd5bc9a405955e67a19a648a
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-04-28 09:59:14 +0200

    UserStory1-ModelClass

---
+++ DR_Projekt/Models/MusicRecord.cs
@@ -0,0 +1,32 @@
+﻿namespace DR_Projekt.Models
+{
+    public class MusicRecord
+    {
+        //Her gemmer vi
+        private string? _title;
+        private int _duration;
+        private string? _artist;
+        private int _year;
+
+        // Her er vores filter
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Hov du glemte at skrive en kunstner?!");
+
+                }
+
+                _title = value;
+            }
+
+        }
+
+        public override string ToString() {
+            return $"{Title}";
+        }
+    }
+}
---
+++ DR_ProjektTests/Models/MusicRecordTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DR_Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DR_Projekt.Models.Tests
+{
+    [TestClass()]
+    public class MusicRecordTests
+    {
+
+        [TestMethod()]
+        public void TestTitle() {
+            //Arrange
+            MusicRecord musicRecord = new MusicRecord();
+            //Act
+     
+            //Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                () => musicRecord.Title = null);
+        }
+
+        [TestMethod()]
+        public void ToStringTest() {
+            Assert.Fail();
+        }
+    }
+}
